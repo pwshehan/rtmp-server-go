6be982bd797cb2aea482ac815961a10f558401c5
Shehan Wijethunga <pwshehan@gmail.com>
2024-06-15 10:18:32 +0530

    feat: start with gin

--- auth.go
+++ auth.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
-		streamKey := r.URL.Query().Get("name")
+	router := gin.Default()
+
+	router.GET("/auth", func(c *gin.Context) {
+		streamKey := c.Query("name")
 
 		validStreamKeys := map[string]bool{
 			"validstreamkey1": true,
@@ -15,17 +18,11 @@ func main() {
 		}
 
 		if validStreamKeys[streamKey] {
-			w.WriteHeader(http.StatusOK)
-			fmt.Fprintf(w, "OK")
+			c.String(http.StatusOK, "OK")
 		} else {
-			w.WriteHeader(http.StatusForbidden)
-			fmt.Fprintf(w, "Invalid stream key")
+			c.String(http.StatusForbidden, "Invalid stream key")
 		}
 	})
 
-	fmt.Println("Starting server on :8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		fmt.Println("Error starting server:", err)
-		return
-	}
+	router.Run(":8081")
 }
