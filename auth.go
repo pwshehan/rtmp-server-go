83478943fb9f09b3256961a93eab1d49870a4de3
Shehan Wijethunga <pwshehan@gmail.com>
2024-06-15 10:01:48 +0530

    feat: auth server

--- auth.go
+++ auth.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	})
+
+	fmt.Println("Starting server on :8081")
+	if err := http.ListenAndServe(":8081", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
 }
